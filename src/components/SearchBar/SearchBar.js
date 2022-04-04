bd746288b63336be43516472ca69498cb047a47b
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-04 17:08:21 -0300

    instalando react icons e estilizando a barra de pesquisa

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './SearchBar.css'
 
+import { GoSearch } from 'react-icons/go'
+import { IconContext } from "react-icons";
+
 function SearchBar ({ data }) {
     return (
         <>
             <div className="searchInput">
-                icone
+                <IconContext.Provider>
+                <GoSearch />
                 <input type="text" />
+                </IconContext.Provider>
             </div>
             <div className="dataResult">
 
