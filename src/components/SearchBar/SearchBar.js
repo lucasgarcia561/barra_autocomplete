242362ff9277f6daba5ad9ef610ba62bfa5edd5c
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-04 16:52:50 -0300

    ajustando as pastas e realizando imports

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './SearchBar.css'
+
+function SearchBar ({ data }) {
+    return (
+        <>
+            <div className="searchInput">
+                icone
+                <input type="text" />
+            </div>
+            <div className="dataResult">
+
+            </div>
+        </>
+    )
+}
+
+export default SearchBar
