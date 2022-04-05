50340f8094b8950bc56b10e64bad143615b307f9
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-05 14:44:12 -0300

    adicionando clear text

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import './SearchBar.css'
 import { GoSearch } from 'react-icons/go'
 import { IconContext } from "react-icons"
+import { AiOutlineClose } from 'react-icons/ai'
 import React, { useEffect, useState } from 'react';
 
 function SearchBar ({ data }) {
@@ -24,6 +25,16 @@ function SearchBar ({ data }) {
         }
     }, [inputSearch])
 
+    function handleClickAutoComplete(value) {
+        setInputSearch(value.title)
+        setFilterSearch([])
+    }
+
+    function clearText() {
+        setInputSearch("")
+        setFilterSearch([])
+    }
+
     return (
         <>
             <div className="searchInput">
@@ -31,13 +42,16 @@ function SearchBar ({ data }) {
                     <GoSearch />
                 <input value={inputSearch} onChange={handleFilter} type="text" 
                 placeholder="Digite sua pesquisa..." />
+
+                {inputSearch !== "" ? <AiOutlineClose onClick={clearText} /> : ""}
                 </IconContext.Provider>
             </div>
 
             {filterSearch !== 0 &&
             <div className="dataResult">
                 {filterSearch.map(value => (
-                    <div key={value.id} className="dataItem">
+                    <div key={value.id} className="dataItem" onClick={() =>
+                    handleClickAutoComplete(value)}>
                         <IconContext.Provider value={{color: "#49708a", size: "22px"}}>
                             <GoSearch />
                         </IconContext.Provider>
