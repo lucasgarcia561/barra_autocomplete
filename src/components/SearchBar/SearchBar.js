33cbc697aa0eafa8c668b120107893b4097beb86
Lucas Garcia <lucasgarcia561@gmail.com>
2022-04-05 13:31:13 -0300

    adicionando filtro de pesquisa

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,45 @@
-import React from "react";
 import './SearchBar.css'
 import { GoSearch } from 'react-icons/go'
-import { IconContext } from "react-icons";
+import { IconContext } from "react-icons"
+import React, { useState } from 'react';
 
 function SearchBar ({ data }) {
+
+    const [inputSearch, setInputSearch]   = useState("")
+    const [filterSearch, setFilterSearch] = useState([])
+
+    const handleFilter = (event) => {
+        setInputSearch(event.target.value)
+
+        const newFilter = data.filter(value => {
+            return value.title.includes(inputSearch)
+        })
+
+        setFilterSearch(newFilter)
+    }
+
     return (
         <>
             <div className="searchInput">
-                <IconContext.Provider value={{color: "#49708a"}}>
-                <GoSearch />
-                <input type="text" placeholder="Digite sua pesquisa..." />
+                <IconContext.Provider value={{color: "#49708a", size: "30px"}}>
+                    <GoSearch />
+                <input value={inputSearch} onChange={handleFilter} type="text" 
+                placeholder="Digite sua pesquisa..." />
                 </IconContext.Provider>
             </div>
-            <div className="dataResult">
 
+            {filterSearch !== 0 &&
+            <div className="dataResult">
+                {filterSearch.map(value => (
+                    <div key={value.id} className="dataItem">
+                        <IconContext.Provider value={{color: "#49708a", size: "22px"}}>
+                            <GoSearch />
+                        </IconContext.Provider>
+                        <p>{value.title}</p>
+                    </div>
+                ))}
             </div>
+            }
         </>
     )
 }
